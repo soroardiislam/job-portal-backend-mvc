77ede8f817b1a2a5d42d52680ebd1e31bc578615
soroardiislam <mdsoroardi602828@gmail.com>
2025-12-20 19:56:11 +0600

    route

--- routes/job.route.js
+++ routes/job.route.js
@@ -1,6 +1,12 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { getAdminJobs, getAllJobs, getJobById, postJob, deleteJob } from "../controllers/job.controller.js";
+import {
+  getAdminJobs,
+  getAllJobs,
+  getJobById,
+  postJob,
+  deleteJob,
+} from "../controllers/job.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +14,6 @@ router.route("/post").post(isAuthenticated, postJob);
 router.route("/get").get(isAuthenticated, getAllJobs);
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
 router.route("/get/:id").get(isAuthenticated, getJobById);
-router.route("/delete/:id").delete( deleteJob);                               
+router.route("/delete/:id").delete(deleteJob);
 
 export default router;
