70f950a3fef16af50273ab15f1149e3b4116f97b
soroardiislam <mdsoroardi602828@gmail.com>
2025-12-20 19:57:21 +0600

    index

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
- import companyRoute from "./routes/company.route.js";
- import jobRoute from "./routes/job.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
@@ -14,31 +14,27 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: [
-        'http://localhost:5173',
-        'https://job-portal-frontend-iota-one.vercel.app'
-    ],
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://job-portal-frontend-iota-one.vercel.app",
+  ],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
 
-
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
- app.use("/api/v1/job", jobRoute);
- app.use("/api/v1/application", applicationRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
